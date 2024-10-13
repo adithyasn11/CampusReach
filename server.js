4e69551cb8b78171fdd05f3e0c10e2d9f8b835a2
adithyasn11 <adithyasn2487@gamil.com>
2024-10-13 16:01:41 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the public directory
+
 app.use(express.static('public'));
 
-// Route for the homepage
+
 app.get('/', (req, res) => {
-    res.sendFile('public/index.html', { root: __dirname }); // Serve the index.html file directly
+    res.sendFile('public/index.html', { root: __dirname });
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
