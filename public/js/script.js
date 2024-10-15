5e2876d68fff994f26149bd752b2793ec4cf295e
adithyasn11 <adithyasn2487@gamil.com>
2024-10-15 20:26:56 +0530

    add more features

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.getElementById('hamburger');
+const sideMenu = document.getElementById('side-menu');
+const closeBtn = document.getElementById('close-btn');
+
+
+hamburger.addEventListener('click', () => {
+    sideMenu.classList.toggle('show');
+});
+
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('show');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        sideMenu.classList.remove('show');
+    }
+});
